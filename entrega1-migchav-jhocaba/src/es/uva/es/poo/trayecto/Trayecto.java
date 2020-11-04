c9ac3ee738f4525d7eea470bf126758ec0a535ff
migchav <miguel.chaveinte@alumnos.uva.es>
2020-11-05 00:33:54 +0100

    Creacioninicial de metodos infoTrayecto y costeTrayecto.(varios TODO)

--- entrega1-migchav-jhocaba/src/es/uva/es/poo/trayecto/Trayecto.java
+++ entrega1-migchav-jhocaba/src/es/uva/es/poo/trayecto/Trayecto.java
@@ -6,6 +6,7 @@ import es.uva.inf.poo.maps.GPSCoordinate;
  * Clase en la que se almacena la información relativa a los viajes realizados 
  * por cada contenedor
  * @author Jhon
+ * @author migchav
  *
  */
 
@@ -38,14 +39,57 @@ public class Trayecto {
 	 */
 	public Trayecto(String muelleOrigen,String puertoOrigen,int fechaIni,String muelleDestino,String puertoDestino,int fechaFin) {
 		//USO DE THIS: Distinguir el atributo del argumento formal
+		//TODO: SET ?? 
 		this.muelleOrigen = muelleOrigen; 
 		this.puertoOrigen = puertoOrigen;
 		this.fechaIni = fechaIni ;
-		this.puertoDestino = puertoDestino;
+		this.muelleDestino=muelleDestino;
 		this.puertoDestino = puertoDestino;
 		this.fechaFin = fechaFin;
 	}
+	/**
+	 * Consulta dato Muelle de origen
+	 * @return el nombre del muelle origen
+	 */
+	public String getMuelleOrigen() {
+		return muelleOrigen;
+	}
+	/**
+	 * Consulta dato Puerto de origen
+	 * @return el nombre del puerte origen
+	 */
+	public String getPuertoOrigen() {
+		return puertoOrigen;
+	}
 	
+	/**
+	 * Consulta dato Muelle de destino
+	 * @return el nombre del muelle origen
+	 */
+	public String getMuelleDestino() {
+		return muelleDestino;
+	}
+	/**
+	 * Consulta dato Puerto de Destino
+	 * @return el nombre del puerto destino
+	 */
+	public String getPuertoDestino() {
+		return puertoDestino;
+	}
+	/**
+	 * Consulta dato de la fecha inicio
+	 * @return el valor de la fecha inicio del trayecto
+	 */
+	public int getFechaIni() {
+		return fechaIni;
+	}
+	/**
+	 * Consulta dato de la fecha fin
+	 * @return el valor de la fecha final del trayecto
+	 */
+	public int getFechaFin() {
+		return fechaFin;
+	}
 	/**
 	 * Conocer si la fecha de fin es mayor a una dada
 	 *  
@@ -57,6 +101,7 @@ public class Trayecto {
 	 */
 	public boolean Fechacorrecta(int fecha) {
 		//TODO: reconocer que una fecha sea incorrecta
+		//TODO: Convertir fechas a numeros
 		if (fecha == 0) {
 			throw new IllegalArgumentException("Fecha nula o fecha incorrecta");
 		}
@@ -78,7 +123,7 @@ public class Trayecto {
 	
 	public double GetDistancia(GPSCoordinate destino) {
 		//Implementar bien la coordenada origen
-		if (destino == null) {
+		if (destino == null) {	//no hace falta no? ya en getDistanceTo
 			throw new IllegalArgumentException("La coordenada no puede ser nula");
 		}
 		
@@ -87,7 +132,32 @@ public class Trayecto {
 		distancia = coordenadaOrigen.getDistanceTo(destino);
 		return distancia;
 	}
-
+	/**
+	 * Agrupa en una cadena la información relativa a la localidad,pais y fecha tanto
+	 * del origen como del destino
+	 * @return cadena que agrupa de forma legible dicha informacion
+	 */
+	public String infoTrayecto() {
+		//localidad y pais en clase puerto
+		return("La locadidad del puerto origen es:"+getLocalidad()+" en el pais de: "+ getPais()+
+				" y la localidad del puerto destino es "+ getLocalidad()+ " que esta en el pais: "
+				+ getPais()+ ". La fecha de inicio del trayecto es:"+ getFechaIni()+" y la fecha "
+				+ "del fin de trayecto es: "+getFechaFin());
+	}
+	
+	/**
+	 * Calcula el coste en euros para un determinado trayecto. El coste viene dado
+	 * por el precio de la milla y precio por dia del trayecto que es dado 
+	 * y la distancia entre los puntos origen y destino.
+	 * @param precioMilla - Coste en euros de 1 unidad de distancia marina.
+	 * @param precioDia - Coste en euros de 1 dia de trayecto.
+	 * @return el coste en euros total del trayecto.
+	 */
+	public int costeTrayecto(int precioMilla,int precioDia) {
+		//TODO: precio negativo y origen,destino???????
+		GPSCoordinate destino = new GPSCoordinate(40,40); //???????????????
+		return precioMilla*origen.GetDistancia(destino)*(getFechaFin()-getFechaIni())*precioDia;
+	}
 }
 
 
