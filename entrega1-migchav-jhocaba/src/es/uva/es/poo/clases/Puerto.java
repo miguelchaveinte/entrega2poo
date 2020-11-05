e6019335ca2cee4f30c9e74391f54eb6c54de2b8
jhocaba <srjhonst@hotmail.com>
2020-11-05 22:53:33 +0100

    Implementación de la clase Contenedor

--- entrega1-migchav-jhocaba/src/es/uva/es/poo/clases/Puerto.java
+++ entrega1-migchav-jhocaba/src/es/uva/es/poo/clases/Puerto.java
@@ -1,5 +1,39 @@
 package es.uva.es.poo.clases;
+import java.util.ArrayList;
+
+/**
+ * Clase que proporciona la gestión de un puerto de una localidad
+ * @author jhocaba
+ * @author migchav
+ */
 
 public class Puerto {
-
+	//Atributos
+	
+	private String país;
+	private String localidad;
+	
+	/**
+	 * Inicializacion sin argumentos
+	 */
+	
+	public Puerto() {
+		
+	}
+	
+	/**
+	 * Inicialización a partir de argumentos
+	 * @param 
+	 */
+	public Puerto(String identidad) {
+		String [] array = identidad.split("-");
+		this.país = array[0];
+		this.localidad = array[1];
+	}
+	
+	
+	
+	
+	
+	
 }
